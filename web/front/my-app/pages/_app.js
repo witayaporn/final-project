adcfdf27092880684a6f952f5acf03d43e1badb1
witayaporn <6410110483@email.psu.ac.th>
2024-03-25 19:17:43 +0700

    change layout content and simple workig

--- web/front/my-app/pages/_app.js
+++ web/front/my-app/pages/_app.js
@@ -4,7 +4,9 @@ import Layout from "@/components/Layout";
 export default function App({ Component, pageProps }) {
   return (
       <Layout>
-      <Component {...pageProps} />
+        <div className="content">
+          <Component {...pageProps} />
+        </div>
       </Layout>
   )
 }
--- web/front/my-app/pages/index.js
+++ web/front/my-app/pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <>
-    <div className="content">
       <h1>Announcement</h1>
-    </div>
     </>
   );
 }
